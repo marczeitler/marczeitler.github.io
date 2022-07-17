9f07616c3df454a8ce0618f1142f413afa3d3d71
Marc <marc.zeitler@tum.de>
2022-07-17 17:36:14 +0200

    Website Update

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	var products = document.querySelectorAll('[data-bss-dynamic-product]');
-
-	for (var product of products) {
-		var param = product.dataset.bssDynamicProductParam;
-		product.dataset.reflowProduct = new URL(location.href).searchParams.get(param)
-	}
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
 }, false);
